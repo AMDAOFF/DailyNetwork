e3af77a7c7c64d2d81c59188aadd233a077058d0
Kristian Kuhrt <kristian.y8@gmail.com>
2019-04-25 20:50:58 +0200

    Added a new folder for images and splittet the css up in multiple files

--- index.php
+++ index.php
@@ -10,9 +10,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+
+    <!-- LINKING ALL OF THE CSS FILES NECCESARRY -->
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/comm_vars.css">
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/main_style.css">
+
+    <!-- FONT INITIALIZATION -->
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet"> 
+
+    <title>DailyNetwork</title>
 </head>
 <body>
-    
+    <header>
+        <header-top>
+        </header-top>
+
+        <header-middle>
+            <h1>
+                <span class="comm-col-1">Daily</span>
+                <span class="comm-col-2">Network</span>
+            </h1>
+        </header-middle>
+
+        <header-buttom>
+        </header-buttom>
+    </header>
 </body>
 </html>
